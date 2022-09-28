6fc8c24b750e016bf2879816dde05cae9c0c8395
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2022-09-27 18:26:17 -0700

    Delete MallardDuck.java

--- MallardDuck.java
+++
@@ -1,9 +0,0 @@
-public abstract class Duck
-{
-	QuackBehavior quackBehavior; //each duck has a reference to something that implements QuackBehavior interface
-
-	public void performQuack()
-	{
-		quackBehavior.quack();
-	}
-}
